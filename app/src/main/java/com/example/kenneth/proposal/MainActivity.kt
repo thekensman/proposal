0fd6c12e1db87e3e90456d2316c12dc038b16974
Kenneth Cross <thekensman@gmail.com>
2019-06-10 00:38:41 -0500

    Adding backgrounds, styles, and icon

--- app/src/main/java/com/example/kenneth/proposal/MainActivity.kt
+++ app/src/main/java/com/example/kenneth/proposal/MainActivity.kt
@@ -2,28 +2,26 @@ package com.example.kenneth.proposal
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.transition.*
 import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
+    var first_scene: Scene? = null
+    var second_scene: Scene? = null
+    var transition_manager: Transition? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-
-    fun toastMe(view: View) {
-        val myToast = Toast.makeText(this, "Hello Toast!", Toast.LENGTH_SHORT)
-        myToast.show()
-    }
-
-    fun countMe(view: View) {
-        val showCountTextView = findViewById<TextView>(R.id.textView2)
-        val countString = showCountTextView.text.toString()
-        var count: Int = Integer.parseInt(countString)
-        count++
-
-        showCountTextView.text = count.toString()
+        transition_manager = TransitionInflater.from(this)
+                .inflateTransition(R.transition.transition)
+        first_scene = Scene.getSceneForLayout(findViewById(R.id.root_container), R.layout.first_scene, this)
+        second_scene = Scene.getSceneForLayout(findViewById(R.id.root_container), R.layout.second_scene, this)
+        TransitionManager.go(second_scene, transition_manager)
+        //TransitionManager.go(first_scene, transition_manager)
     }
 }
